cabcee9fe70c8bc1521b264971d4a32c34431d39
SauravKulkarni3999 <160947277+SauravKulkarni3999@users.noreply.github.com>
2025-04-18 17:48:28 -0700

    Create data_loader.py

---
+++ notebooks/data_loader.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# Load preprocessed datasets
+adult_yearly_df = pd.read_csv("C:\\Users\\HP\\Desktop\\Learning Interests\\Projects\\AMR Surveillance Dashboard\\data\\adult_yearly_df.csv")
+pediatric_yearly_df = pd.read_csv("C:\\Users\\HP\\Desktop\\Learning Interests\\Projects\\AMR Surveillance Dashboard\\data\\pediatric_yearly_df.csv")
+
+amu_data = pd.read_csv("C:\\Users\\HP\\Desktop\\Learning Interests\\Projects\\AMR Surveillance Dashboard\\data\\amu_data.csv")
+
+df_cluster = pd.read_csv("C:\\Users\\HP\\Desktop\\Learning Interests\\Projects\\AMR Surveillance Dashboard\\data\\df_cluster.csv")
+
+feature_importance_df = pd.read_csv("C:\\Users\\HP\\Desktop\\Learning Interests\\Projects\\AMR Surveillance Dashboard\\data\\feat_importance_df.csv")
